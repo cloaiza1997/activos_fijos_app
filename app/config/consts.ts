e7a8854b9811b8250d077c6d4367dd0600f504ec
Cristian Loaiza <kla-1997@hotmail.com>
2022-02-10 00:27:41 -0500

    master - Readme

--- app/config/consts.ts
+++ app/config/consts.ts
@@ -1,2 +1,4 @@
 export const URL_BACK =
   "http://192.168.0.44/Desarrollo/proyecto_grado/activos_fijos_back/public/api"
+// export const URL_BACK =
+//   "http://192.168.1.56/Desarrollo/proyecto_grado/activos_fijos_back/public/api"
